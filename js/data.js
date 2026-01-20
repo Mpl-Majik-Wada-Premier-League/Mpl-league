6ca557a46527b907ae25f40079722d778173ed41
Mpl-Majik-Wada-Premier-League <abhijeetmajik18@gmail.com>
2026-01-20 12:33:12 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,13 +1,3 @@
-async function loadSeason(year){
-  try{
-    const res = await fetch(`data/${year}.json`);
-    if(!res.ok) return {matches: []}; // Empty if no data yet
-    return await res.json();
-  } catch(e){
-    return {matches: []};
-  }
-}
-
-function getAvailableYears(){
-  return ['2026','2027']; // Add more future years as needed
+{
+  "2026": []
 }
